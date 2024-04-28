80b7122be3ef13d66fde1c490958c8bcfde48eae
user <1296800094@qq.com>
2024-04-28 15:54:34 +0800

    配置请求路径

--- src/requestConfig.ts
+++ src/requestConfig.ts
@@ -5,7 +5,7 @@
  */
 export const requestConfig: RequestConfig = {
   // 请求地址
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:8080/api',
   // 允许携带cookie
   withCredentials: true,
   // 请求拦截器
